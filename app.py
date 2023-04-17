81c89ec5674c8684ad84c5bd3a4212313569a8ca
unknown <luizhgirolometto@hotmail.com>
2023-04-17 00:51:34 -0300

    insert ajuste

--- app.py
+++ app.py
@@ -23,46 +23,83 @@ USERNAME = config.get('default', 'username')
 PASSWORD = config.get('default', 'password')
 DATABASE = config.get('default', 'database')
 
+##########################################################################################################
+
+
+
 class User:
-    def __init__(self, tel):
+    def __init__(self,idvaga,tel,email):
         self.tel = tel
-        self.email = None
+        self.email = email
+        self.idvaga = idvaga
 
 
 
-@bot.message_handler(commands=["cadastrar"])
+@bot.message_handler(commands=["candidatar"])
 def insert(mensagem):
     # Criar o formulário com os campos personalizados
 
     msg = bot.send_message(mensagem.chat.id, "Vamos começar?, preencha o formulário abaixo \n\n"
-                                             "Primeiro o seu telefone?")
+                                             "Primeiramente digite codigo da vaga desejada")
+    bot.register_next_step_handler(msg, get_vaga)
+
+def get_vaga(mensagem):
+    # Pedir o telefone
+    msg = bot.send_message(mensagem.chat.id, "Qual é o seu telefone? ex: 4699990000")
     bot.register_next_step_handler(msg, get_tel)
+    User.idvaga = mensagem.text
+
+    print(User.idvaga)
+
+
 
 def get_tel(mensagem):
-    # Pedir o telefone
     tel = mensagem.text
-    msg = bot.send_message(mensagem.chat.id, "Qual é o seu nome?")
-    bot.register_next_step_handler(msg, get_email)
-
-    print(tel)
-
-
-def get_email(mensagem):
     # Pedir o email do usuário
     msg = bot.send_message(mensagem.chat.id, "Qual é o seu email?")
-    bot.register_next_step_handler(msg, get_telefone)
-
-
-def get_telefone(mensagem):
-    # Pedir o telefone do usuário
-    msg = bot.send_message(mensagem.chat.id, "Qual é o seu telefone?")
     bot.register_next_step_handler(msg, end)
-
+    User.tel = mensagem.text
+    print(User.tel)
 
 def end(mensagem):
+
     # Exibir uma mensagem de agradecimento e finalizar o formulário
     bot.send_message(mensagem.chat.id, "Obrigado por preencher o formulário.")
+    User.email = mensagem.text
+    print(User.email)
 
+    insert_data(mensagem)
+
+
+def insert_data(mensagem):
+
+
+    fullname = mensagem.chat.first_name + mensagem.chat.last_name
+
+    vaga_id = int(User.idvaga)
+    nome = fullname
+    email = User.email
+    telefone = User.tel
+
+    # create the tuple with all the params interted by the user
+    params = (vaga_id,nome,email,telefone)
+
+    # Create the UPDATE query, we are updating the product with a specific id so we must put the WHERE clause
+    sql_command = "INSERT INTO candidaturas VALUES (%s, %s, %s, %s);"  # the initial NULL is for the AUTOINCREMENT id inside the table
+    crsr.execute(sql_command, params)  # Execute the query
+    conn.commit()  # Commit the changes
+
+    # If at least 1 row is affected by the query we send specific messages
+    if crsr.rowcount < 1:
+        texto = "Algo deu errado! tente novamente"
+        bot.send_message(mensagem.chat.id, texto)
+    else:
+        texto = "Tudo certo! inserido corretamente"
+        bot.send_message(mensagem.chat.id, texto)
+
+
+
+#####################################################################################################################
 
 @bot.message_handler(commands=["vagas"])
 def vagas(mensagem):
@@ -77,6 +114,8 @@ def vagas(mensagem):
         texto = "Nenhuma vaga encontrada."
         bot.send_message(mensagem.chat.id, formatting.format_text(formatting.mbold(texto)),parse_mode='MarkdownV2')
 
+
+########################################################################################################################
 
 #funcao que le a msg de entrada
 def start(mensagem):
@@ -93,8 +132,11 @@ def responder(mensagem):
 
         chat_id = mensagem.chat.id
         markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-        markup.add('/vagas','/cadastrar','/consulta')
+        markup.add('/vagas','/candidatar','/consulta')
         msg = bot.send_message(mensagem.chat.id,'Escolha uma das opções abaixo:',reply_markup=markup)
+
+        print(mensagem.chat.first_name, mensagem.chat.last_name)
+
 
 def create_database(query):
     try:
